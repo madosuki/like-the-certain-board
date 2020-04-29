fd929b216e33bab274f5a11bea12ebe89f6b568f
madosuki <yurikasamaaikatsu@gmail.com>
2020-04-29 16:40:37 +0900

    test add run.lisp is run server script.

---
+++ src/run.lisp
@@ -0,0 +1,19 @@
+(ql:quickload 'clack)
+(ql:quickload 'cl-fad)
+
+(defparameter *server* nil)
+
+(defun run (name)
+    (let ((application name))
+    (if (cl-fad:file-exists-p application)
+        (progn
+          (setq *server* (clack:clackup application)))
+        (format t "not found this file."))))
+
+(defun end-server ()
+  (clack:stop *server*))
+
+(defun main (&rest argv)
+  (let ((tmp (car argv)))
+    (run tmp))
+  )
