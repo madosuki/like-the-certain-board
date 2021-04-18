8a110fdbdd8bef1b6d5116b10b9a3425cb3b5db6
madosuki <yurikasamaaikatsu@gmail.com>
2021-04-18 10:47:50 +0900

    update docckerRun.sh

--- dockerRun.sh
+++ dockerRun.sh
@@ -3,5 +3,6 @@
 export BOARD_SETTINGS_PATH=/roswell/settings/
 export KAKOLOG_HTML_DIR_PATH=/roswell/html/
 export DAT_DIR_PATH=/roswell/dat/
+export APP_ENV=production
 
-cd /root/.roswell/local-projects/like-the-certain-board/ && APP_ENV=production clackup --server :fcgi --address 0.0.0.0 --port 8888 ./app.lisp
+cd /root/.roswell/local-projects/like-the-certain-board/ && clackup --server :fcgi --address 0.0.0.0 --port 8888 ./app.lisp
