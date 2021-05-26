735988d8856f4deffc62f27bdbb1beb32046a19e
madosuki <yurikasamaaikatsu@gmail.com>
2021-05-26 10:49:57 +0900

    update dockerRun.sh, add update library process

--- dockerRun.sh
+++ dockerRun.sh
@@ -6,4 +6,12 @@ export KAKOLOG_DAT_DIR_PATH=/roswell/kakolog/dat/
 export DAT_DIR_PATH=/roswell/dat/
 export APP_ENV=production
 
+ros update git lack
+ros update git ningle
+ros update git clack
+ros update git generate-like-certain-board-strings
+ros update git cl-markup
+
+ros update quicklisp
+
 cd /root/.roswell/local-projects/like-the-certain-board/ && clackup --server :fcgi --address 0.0.0.0 --port 8888 ./app.lisp
