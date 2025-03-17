9ee2c7285f0780685dad3f1467f2cc1d30371797
madosuki <yurikasamaaikatsu@gmail.com>
2025-03-17 17:40:08 +0900

    fix misunderstand

--- app.lisp
+++ app.lisp
@@ -49,23 +49,15 @@
           :expires 1800))
  (:csrf
   :one-time t
-  ;; block-app section is set behaviour when passed invalid csrf token.
   :block-app (lambda (app env)
-               (declare (ignore app)
-                        (ignore env))
-               '(400
-                 (:content-type "text/plain"
-                  :content-length 9)
-                 ("bad request"))
-               ;; (let ((user-agent (gethash "user-agent" (getf env :headers))))
-               ;;   (if (cl-ppcre:scan "^Monazilla/1.00" user-agent)
-               ;;       (funcall app env)
-               ;;       '(400
-               ;;         (:content-type "text/plain"
-               ;;          :content-length 9)
-               ;;         ("bad request"))
-               ;;       ))
-               ))
+               (let ((user-agent (gethash "user-agent" (getf env :headers))))
+                 (if (cl-ppcre:scan "^Monazilla/1.00" user-agent)
+                     (funcall app env)
+                     '(400
+                       (:content-type "text/plain"
+                        :content-length 9)
+                       ("bad request"))
+                     ))))
  (if (productionp)
      nil
      (lambda (app)
