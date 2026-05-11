078d66bfae6ffc3d6e11e9c7536c9ec7a2744e55
madosuki <yurikasamaaikatsu@gmail.com>
2026-05-12 00:55:23 +0900

    update sql files

--- docker/postgresql/sql/kakolog.sql
+++ docker/postgresql/sql/kakolog.sql
@@ -1,1 +1,7 @@
-create table if not exists kakolog (id serial, unixtime bigint not null, title text not null, "board-id" integer not null, primary key(id, unixtime))
+create table if not exists kakolog (
+id serial,
+unixtime bigint not null,
+title text not null,
+"board-id" integer not null,
+primary key(id, unixtime),
+constraint fk_kakolog foreign key ("board-id") references "board-list" (id))
--- docker/postgresql/sql/threads.sql
+++ docker/postgresql/sql/threads.sql
@@ -1,1 +1,12 @@
-create table if not exists threads (id serial, title text not null, "create-date" timestamp not null, "last-modified-date" timestamp not null, "last-rise-date" timestamp not null, "res-count" integer not null default 1, unixtime bigint, "max-line" integer default 1000 not null, "board-id" integer not null, primary key(id, unixtime))
+create table if not exists threads (
+id serial primary key,
+title text not null,
+"create-date" timestamp not null,
+"last-modified-date" timestamp not null,
+"last-rise-date" timestamp not null,
+"res-count" integer not null default 1,
+unixtime bigint not null,
+"max-line" integer default 1000 not null,
+"board-id" integer not null,
+constraint uq_threads_unixtime unique ("board-id", unixtime)
+constraint fk_threads foreign key ("board-id") references "board-list" (id))
