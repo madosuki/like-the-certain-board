1d561e28110f63f7b576b7df80daa37f8292c6de
madosuki <yurikasamaaikatsu@gmail.com>
2026-05-12 14:11:04 +0900

    fix sql

--- create_board.sh
+++ create_board.sh
@@ -1,3 +1,5 @@
-#!/bin/sh
+#!/usr/bin/env sh
 
 psql -h localhost -U user mysite -c "insert into \"board-list\" (name, \"url-name\", \"default-name\") values('やる夫とLisp', 'lisp', '名無しさん');"
+
+psql -h localhost -U user mysite -c "insert into \"board-terms\" (\"board-id\", body) values(1, '規約');"
